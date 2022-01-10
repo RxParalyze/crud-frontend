03dcffff694d329f94373bb8bbbe56ebdbfbda4d
RxParalyze <c17phillip.smart@gmail.com>
2022-01-10 11:57:03 -0500

    Began EnterpriseDB Tutorial

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,8 +1,53 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+//Beginning of EnterpriseDB Tutorial
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ message: 'Hello World' }))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('postgres://postgres:password@127.0.0.1:5432/crud')
+
+sequelize
+
+.authenticate()
+
+.then(() => {
+
+console.log('Connection has been established successfully.');
+
+})
+
+.catch(err => {
+
+console.error('Unable to connect to the database:', err);
+
+});
+
+//End of EnterpriseDB Tutorial
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +59,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
