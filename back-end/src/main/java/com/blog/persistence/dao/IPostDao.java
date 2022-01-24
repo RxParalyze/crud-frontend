1ce0a699adc9d1743484701ce1d4280817bfe2f3
RxParalyze <c17phillip.smart@gmail.com>
2022-01-24 14:04:04 -0500

    moved back-end

--- back-end/src/main/java/com/blog/persistence/dao/IPostDao.java
+++
@@ -1,9 +0,0 @@
-package com.blog.persistence.dao;
-
-import com.blog.persistence.model.Post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPostDao extends JpaRepository<Post, Integer> {
-
-}
--- back-end/src/main/java/com/blog/persistence/dao/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.blog.persistence.dao;
-
-import com.blog.persistence.model.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserDao extends JpaRepository<User, Integer> {
-
-}
