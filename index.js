5cfa781058a76a8b78db98695da1485c301fde11
RxParalyze <c17phillip.smart@gmail.com>
2022-01-26 11:46:18 -0500

    added corsOptions

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const express = require('express');
 const path = require('path');
 const PORT = `${publicRuntimeConfig.PORT}` || 3000;
 
+var corsOptions = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+
 app = express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(cors(corsOptions))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-app.use(cors());
-
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
